8ea781bbb7a807b56e97beefd1845831d0aaeb74
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-03 21:18:36 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -32,7 +32,7 @@ export default class MovieCard extends Component {
   render() {
     return (
       <div className="movie-card">
-        <CardFront poster={posterMap[this.props.poster] />
+        <CardFront poster={posterMap[this.props.poster]} />
         <CardBack />
       </div>
     )
