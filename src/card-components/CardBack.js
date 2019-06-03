64dae5a5484688cd0110968fa8a4c4b01aa64390
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-03 21:28:15 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -21,7 +21,7 @@ export default class CardBack extends Component {
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">{this.props.title}</h3>
         <span />
         { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
         {this.generateRatingElement()}
